8a6466e61a709bab2d49aa9c8b4a80efb5147f4f
torone89 <torone89>
2022-08-30 19:13:19 +0200

    Snack 1 Complete

--- snack1.php
+++ snack1.php
@@ -27,7 +27,7 @@ $partite = [
     ],
 ];
 
-var_dump($partite);
+// var_dump($partite);
 ?>
 
 <!DOCTYPE html>
@@ -52,6 +52,16 @@ var_dump($partite);
             <h5 class="fw-bold text-center">Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
                 Olimpia Milano - Cantù | 55-60</h5>
 
+            <ul class="list-unstyled">
+                <?php for ($i = 0; $i < count($partite); $i++) : ?>
+                    <li class="py-2 fs-6 text-uppercase">
+
+                        <?php
+                        echo $partite[$i]['Team_Casa'] . ' - ' . $partite[$i]['Team_Ospite'] . ' | ' . $partite[$i]['Point_Team_Casa'] . ' - ' . $partite[$i]['Point_Team_Ospite'];
+                        ?>
+                    </li>
+                <?php endfor; ?>
+            </ul>
 
 
 
