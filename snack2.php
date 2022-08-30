51e609b07c56dec27cf666b2c849d30d9b86b415
torone89 <torone89>
2022-08-30 17:28:41 +0200

    Snack 2 Completo

--- snack2.php
+++ snack2.php
@@ -1,5 +1,7 @@
 <?php
-// Controlliamo se la mail contenga un punto e una chiocciola
+
+
+// VARIABILI
 $name = $_GET['name'] ?? '';
 $mail = $_GET['mail'] ?? '';
 $age = $_GET['age'] ?? '';
@@ -11,13 +13,7 @@ $age = $_GET['age'] ?? '';
 // } else {
 //     echo 'Mail valida';
 // }
-
-
 ?>
-
-
-
-
 <!DOCTYPE html>
 <html lang=“en”>
 
@@ -29,39 +25,35 @@ $age = $_GET['age'] ?? '';
     <!-- IMPORT BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-
-
 </head>
 
 <body>
     <main>
         <!-- SNACK 2
  -->
-        <div class="container text-center mt-5">
+        <div class="container text-center my-5">
             <h5 class="fw-bold text-center">Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
                 che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
                 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”</h5>
-            <!-- stampo la variabile text -->
 
 
-            <form action="index.php" method="GET">
-                <input type="text" name="name" placeholder="name">
-                <input type="email" name="mail" placeholder="email">
-                <input type="text" name="age" placeholder="age">
-                <button type="submit">submit</button>
+            <form action="" method="GET" class="mt-5">
+                <input type="text" name="name" placeholder="Nome">
+                <input type="email" name="mail" placeholder="Email">
+                <input type="text" name="age" placeholder="Età">
+                <button type="submit">Inserisci</button>
 
-                <!-- VERIFICO SE  LA STRINGA NAME > 3 & MAIL CONTENGA UN PUNTO/@ e AGE SIA UN NUMERO  -->
                 <?php
-
+                //VERIFICO SE LE STRINGHE SN VUOTE
                 if (empty($name) && empty($mail) && empty($mail)) {
-                    echo "Inserisci i dati";
+                    echo "<h6 class='fw-bold text-uppercase mt-3'>Nessun Dato Inserito</h6>";
                 } else {
-                    if ((strlen($name) > 3) && (strpos($mail, '@') > 0 && strpos($mail, '.') > 3) && (is_numeric($age))) {
-                        echo "accesso riuscito";
-                        //VERIFICO SE LE STRINGHE SN VUOTE
+                    //VERIFICO SE  LA STRINGA NAME > 3 & MAIL CONTENGA UN PUNTO/@ e AGE SIA UN NUMERO  
 
+                    if ((strlen($name) > 3) && (strpos($mail, '@') > 0 && strpos($mail, '.') > 3) && (is_numeric($age))) {
+                        echo "<h6 class='text-success mt-3'>accesso riuscito</h6>";
                     } else {
-                        echo "accesso negato";
+                        echo "<h6 classe='text-danger mt-3'>accesso negato</h6>";
                     }
                 } ?>
 
