92dfe7a35e885bea7894ead9c67bcd32134b2bb4
torone89 <torone89>
2022-08-30 17:53:04 +0200

    Creo Array Associativo

--- snack1.php
+++ snack1.php
@@ -1,21 +1,64 @@
+<?php
+// CREO UN ARRAY ASSOCIATIVO
+$partite = [
+    [
+        'Team_Casa' => 'Venezuela',
+        'Team_Ospite' => 'Repubblica Dominicana',
+        'Point_Team_Casa' => '76',
+        'Point_Team_Ospite' => '72'
+    ],
+    [
+        'Team_Casa' => 'Brasile',
+        'Team_Ospite' => 'Messico',
+        'Point_Team_Casa' => '72',
+        'Point_Team_Ospite' => '82'
+    ],
+    [
+        'Team_Casa' => 'Uruguay',
+        'Team_Ospite' => 'Portorico',
+        'Point_Team_Casa' => '78',
+        'Point_Team_Ospite' => '70'
+    ],
+    [
+        'Team_Casa' => 'Panama',
+        'Team_Ospite' => 'Canada',
+        'Point_Team_Casa' => '50',
+        'Point_Team_Ospite' => '106'
+    ],
+];
+
+var_dump($partite);
+?>
+
 <!DOCTYPE html>
 <html lang=“en”>
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack PHP</title>
     <!-- IMPORT BOOTSTRAP -->
-    <link href=“https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css” rel=“stylesheet” integrity=“sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx” crossorigin=“anonymous”>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 </head>
 
 <body>
-   <main>
-<!-- SNACK 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
+    <main>
+        <!-- SNACK 1
+ -->
 
-</main>
+        <div class="container text-center my-5">
+            <h5 class="fw-bold text-center">Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+                Olimpia Milano - Cantù | 55-60</h5>
+
+
+
+
+
+
+        </div>
+    </main>
 </body>
+
 </html>
